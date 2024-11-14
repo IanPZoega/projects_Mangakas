98a30a81f6d1593805b1658071d1f52c69d2fae9
IanPZ <169824627+IanPZoega@users.noreply.github.com>
2024-11-14 01:54:32 -0300

    configuração de tab e reset do carrossel

--- js/signup.js
+++ js/signup.js
@@ -3,12 +3,12 @@ const images = document.getElementById('images');
 const totalImages = document.querySelectorAll('.image').length;
 
 function changeImage() {
-    index++;
-    if (index >= totalImages) {
-        index = 0;
-    }
-    
-    images.style.transform = `translateX(${-index * 100}%)`;
+  index++;
+  if (index >= totalImages) {
+    index = 0;
+  }
+
+  images.style.transform = `translateX(${-index * 100}%)`;
 }
 
 setInterval(changeImage, 3000);
