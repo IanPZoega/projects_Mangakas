32e6ff7b31a2b7a429028e3b388cf706c5b5d777
IanPZ <169824627+IanPZoega@users.noreply.github.com>
2024-11-15 13:26:54 -0300

    php e bd

---
+++ bd/mangakas-BD.sql
@@ -0,0 +1,12 @@
+create database mgks;
+use mgks;
+
+create table users
+(
+id int PRIMARY KEY auto_increment,
+username varchar(30) NOT NULL UNIQUE,
+senha varchar(30) NOT NULL
+);
+
+select * from users;
+drop table users;
