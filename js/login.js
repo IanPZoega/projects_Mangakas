119d631225855e7d87fb5f2b005f27308615ee21
IanPZ <169824627+IanPZoega@users.noreply.github.com>
2024-11-24 22:43:53 -0300

    esconder senha e começo das notificações

---
+++ js/login.js
@@ -0,0 +1,15 @@
+const passField = document.querySelector('#password');
+const showBtn = document.querySelector('#show-btn');
+
+showBtn.onclick = () => {
+  if (passField.type === 'password') {
+    passField.type = 'text';
+    showBtn.classList.add('hide-btn');
+    showBtn.classList.replace('fa-eye', 'fa-eye-slash');
+
+  } else {
+    passField.type = 'password';
+    showBtn.classList.remove('hide-btn');
+    showBtn.classList.replace('fa-eye-slash', 'fa-eye');
+  }
+};
--- js/signup.js
+++ js/signup.js
@@ -12,3 +12,36 @@ function changeImage() {
 }
 
 setInterval(changeImage, 3000);
+
+//----------------------------------------------------------------
+
+const passField1 = document.querySelector('#password');
+const passField2 = document.querySelector('#confirm-password');
+const showBtn1 = document.querySelector('#show-btn1');
+const showBtn2 = document.querySelector('#show-btn2');
+
+showBtn1.onclick = () => {
+  if (passField1.type === 'password') {
+    passField1.type = 'text';
+    showBtn1.classList.add('hide-btn');
+    showBtn1.classList.replace('fa-eye', 'fa-eye-slash');
+
+  } else {
+    passField1.type = 'password';
+    showBtn1.classList.remove('hide-btn');
+    showBtn1.classList.replace('fa-eye-slash', 'fa-eye');
+  }
+};
+
+showBtn2.onclick = () => {
+  if (passField2.type === 'password') {
+    passField2.type = 'text';
+    showBtn2.classList.add('hide-btn');
+    showBtn2.classList.replace('fa-eye', 'fa-eye-slash');
+
+  } else {
+    passField2.type = 'password';
+    showBtn2.classList.remove('hide-btn');
+    showBtn2.classList.replace('fa-eye-slash', 'fa-eye');
+  }
+};
